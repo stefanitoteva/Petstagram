522fcebb2aa765bee04c63cc7581db800dbdf656
Stefani Toteva <stefani.toteva13@gmail.com>
2023-06-15 23:33:07 +0300

    add login and register pages

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
+const userController = require('./controllers/userController');
 
 
 router.use(homeController);
+router.use('/users', userController);
 
 
 
