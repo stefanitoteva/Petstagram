3cc33ea608554f587ddcc1813713620848825a81
Stefani Toteva <stefani.toteva13@gmail.com>
2023-06-17 10:49:46 +0300

    setup authentication functionality

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -9,7 +9,9 @@ exports.auth = async (req, res, next) => {
         try {
             const decodedToken = await jwt.verify(token, SECRET);
 
-            req.user = decodedToken
+            req.user = decodedToken;
+            res.locals.user = decodedToken;
+            res.locals.isAuthenticated = true;
 
             next();
         } catch (error) {
@@ -21,4 +23,12 @@ exports.auth = async (req, res, next) => {
 
         next();
     }
+}
+
+exports.isAuth = (req, res, next) => {
+    if(!req.user) {
+        return res.redirect('/users/login');
+    }
+
+    next();
 }
