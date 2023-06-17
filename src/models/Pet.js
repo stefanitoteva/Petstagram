1698b4931458186f71ca162042a016c93a8ce514
Stefani Toteva <stefani.toteva13@gmail.com>
2023-06-17 21:49:53 +0300

    create pet model

---
+++ src/models/Pet.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const User = require('./User');
+
+const petSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Pet name is required!']
+    },
+    image: {
+        type: String,
+        required: [true, 'Image is required!']
+    },
+    age: {
+        type: Number,
+        required: [true, 'Age is required!']
+    },
+    description: {
+        type: String,
+        required: [true, 'Description is required!']
+    },
+    location: {
+        type: String,
+        required: [true, 'Location is required!']
+    },
+    commentList: [{
+        userID: {
+            type: mongoose.Types.ObjectId,
+            ref: 'User'
+        },
+        comment: String,
+    }],
+    owner: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }
+
+})
