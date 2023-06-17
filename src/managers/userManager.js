a8218fb40e51bf784b8101861f50da087759e17b
Stefani Toteva <stefani.toteva13@gmail.com>
2023-06-17 10:30:04 +0300

    create auth middleware

--- src/managers/userManager.js
+++ src/managers/userManager.js
@@ -2,7 +2,7 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('../lib/jwt');
 
-const SECRET = '14c7355cad66f20f2f43451158b46833871697d68d1576340600dd50877e56bf';
+const { SECRET } = require('../config/config');
 
 exports.register = async (userData) => {
     const user = await User.findOne({ username: userData.username });
---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,22 @@
+const jwt = require('../lib/jwt');
+
+const { SECRET } = require('../config/config');
+
+exports.auth = async (req, res, next) => {
+    const token = req.cookies['token'];
+
+    if (token) {
+        try {
+            const decodedToken = await jwt.verify(token, SECRET);
+
+            req.user = decodedToken
+
+            next();
+        } catch (error) {
+            res.clearCookie('token')
+        }
+    } else {
+
+        next();
+    }
+}
